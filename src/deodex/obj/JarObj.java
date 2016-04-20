eb6e3b6eb725b0a95c9c838d0c29e1f039169b2b
lord-ralf-adolf <rachidboudjelida@gmail.com>
2016-04-20 12:45:15 +0100

    remove verbose logging to std from command line

--- src/deodex/obj/JarObj.java
+++ src/deodex/obj/JarObj.java
@@ -74,7 +74,6 @@ public class JarObj {
 			FilesUtils.copyFile(S.DUMMY_JAR, this.origJar);
 		}
 		this.tmpFolder = new File(tmpFolder.getAbsolutePath() + File.separator + this.absoluteName);
-		System.out.print("JArObj temp folder created ?" + this.tmpFolder.mkdirs() + tmpFolder.mkdirs());
 		if (!this.tmpFolder.exists())
 			return false;
 		tmpCompodex = new File(this.tmpFolder.getAbsolutePath() + File.separator + this.odexFile.getName());
